d6abc04545ec9ca15c4b6af448620c5c0067fa49
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2024-01-25 12:58:26 -0300

    Refatoração de todo o Código

--- scripts/script.js
+++ scripts/script.js
@@ -1,104 +1,80 @@
+const btnCriptografar = document.querySelector('.btn-cript')
+const btnDescriptografar = document.querySelector('.btn-descript')
+const btnCopiar = document.querySelector('.btn-copiar')
 
-let cript = document.querySelector('button:nth-child(1)')
-let descript = document.querySelector('.decript')
-let copiar = document.querySelector('.btn-copiar')
-
-let divResultadoImg = document.querySelector('.container__resultado__conteudo')
-let divResultado = document.querySelector('.decrip-encrip')
-
-// apenas letras minusculas
-document.querySelector('.input-texto').addEventListener('keyup', () => {
-    let text = document.querySelector('.input-texto').value
-
-    if(text == '') {
-        divResultadoImg.style.display = 'flex'
-        divResultado.style.display = 'none'
-        btnDesabled()
-    } else {
-
-        text = text.toLowerCase()
-        document.querySelector('.input-texto').value = text
-    
-        // mostrarResultado()
-        btnDesabled()
-    }
-
-   
-})
+const primeiroTexto = document.querySelector('.primeiro-texto')
+const textoResult = document.querySelector('.txt-result')
 
 
-cript.addEventListener('click', () => {
-    let text = document.querySelector('.input-texto').value
-
-    if(text) {
-        mostrarResultado()
+btnCriptografar.addEventListener('click', () => {
+    if(primeiroTexto.value != '') {
         criptografar()
-    } else {
-        divResultadoImg.style.display = 'flex'
-        divResultado.style.display = 'none'
+        mostraResultado()
     }
 })
 
-descript.addEventListener('click', () => {
+btnDescriptografar.addEventListener('click', () => {
     descriptografar()
 })
 
-copiar.addEventListener('click', () => {
+btnCopiar.addEventListener('click', () => {
     copy()
 })
 
-function mostrarResultado() {
 
-    divResultadoImg.style.display = 'none'
-    divResultado.style.display = 'flex'
-}
+primeiroTexto.addEventListener('keyup', () => {
+    primeiroTexto.value = primeiroTexto.value.toLowerCase()
+
+    if(!primeiroTexto.value) {
+        esconderResultado()
+    }
+})
+
 
 function criptografar() {
-    let text = document.querySelector('.input-texto').value
+    let texto = document.querySelector('.primeiro-texto').value
 
-    text = text.replace(/e/g, "enter");
-    text = text.replace(/i/g, "imes");
-    text = text.replace(/a/g, "ai");
-    text = text.replace(/o/g, "ober");
-    text = text.replace(/u/g, "ufat");
+    texto = texto.replace(/e/g, "enter");
+    texto = texto.replace(/i/g, "imes");
+    texto = texto.replace(/a/g, "ai");
+    texto = texto.replace(/o/g, "ober");
+    texto = texto.replace(/u/g, "ufat");
 
-    document.querySelector('#resultado-decrip').value = text;
+    document.querySelector('.txt-result').value = texto;
 }
 
 function descriptografar() {
-    let text = document.querySelector('#resultado-decrip').value
+    let texto = document.querySelector('.txt-result').value
 
-    text = text.replace(/ufat/g, "u");
-    text = text.replace(/ober/g, "o");
-    text = text.replace(/ai/g, "a");
-    text = text.replace(/imes/g, "i");
-    text = text.replace(/enter/g, "e");
+    texto = texto.replace(/ufat/g, "u");
+    texto = texto.replace(/ober/g, "o");
+    texto = texto.replace(/ai/g, "a");
+    texto = texto.replace(/imes/g, "i");
+    texto = texto.replace(/enter/g, "e");
 
-    document.querySelector('#resultado-decrip').value = text;
+    document.querySelector('.txt-result').value = texto;
+}
+
+function mostraResultado() {
+    
+    let containerImg = document.querySelector('.container-resultado')
+    let containerResult = document.querySelector('.container-result-cript')
+
+    containerImg.style.display = 'none'
+    containerResult.style.display = 'flex'
+}
+
+function esconderResultado() {
+    let containerImg = document.querySelector('.container-resultado')
+    let containerResult = document.querySelector('.container-result-cript')
+
+    containerImg.style.display = 'flex'
+    containerResult.style.display = 'none'
 }
 
 function copy() {
-    let copyText = document.querySelector("#resultado-decrip");
+    let copyText = document.querySelector(".txt-result");
     copyText.select();
     document.execCommand("copy");
+    alert(`Resultado copiado !!!\n"${copyText.value}"`)
 }
-
-function btnDesabled() {
-    console.log('btn-disabed')
-    let texto = document.querySelector('.input-texto')
-    let btnCript = document.querySelector('button:nth-child(1)')
-    if(texto.value == '') {
-        // btnCript.classList.remove('cript')
-        btnCript.classList.add('btn-desabled')
-        btnCript.setAttribute('disabled', true)
-
-    } else {
-        btnCript.classList.remove('btn-desabled')
-        btnCript.classList.add('cript')
-        btnCript.removeAttribute('disabled')
-    }
-}
-
-btnDesabled()
-
-
